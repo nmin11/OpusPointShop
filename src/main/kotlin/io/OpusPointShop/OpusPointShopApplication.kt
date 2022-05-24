40d0a732ec834190a4972a9e0c359944a342796c
nmin11 <nmin1124@gmail.com>
2022-05-24 16:19:29 +0900

    Initial commit

---
+++ src/main/kotlin/io/OpusPointShop/OpusPointShopApplication.kt
@@ -0,0 +1,11 @@
+package io.OpusPointShop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OpusPointShopApplication
+
+fun main(args: Array<String>) {
+	runApplication<OpusPointShopApplication>(*args)
+}
---
+++ src/test/kotlin/io/OpusPointShop/OpusPointShopApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.OpusPointShop
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OpusPointShopApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
